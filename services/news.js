6c01224e05151878d6e3a61a251d610fee61cb42
sawyerthemiller <dukes.of.ozaukee@gmail.com>
2026-07-14 12:00:27 -0500

    finalised regex cleanups

--- services/news.js
+++ services/news.js
@@ -26,6 +26,7 @@
     return title
       .replace(/A\.I\.?/gi, 'AI')
       .replace(/\s*\[([^\]]+)\]/g, ' $1')
+      .replace(/[-+\u2013\u2014\u2212]\s*\$/g, '')
       .replace(/["'’”“@:;+\u00B1$?,\u235C\u2192]/g, '')
       .replace(/[\u2013\u2014_]/g, '-')
       .replace(/\.{4,}$/, '...')
